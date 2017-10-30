891c456a03e496a9e2936f710aa92da2474f2cfa
jay bengco <jay.bengco@tooltwist.com>
2017-10-30 14:29:01 +0800

    UPDATE docker

--- dockerize/run.sh
+++ dockerize/run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+#student application 
+#export ="???@??.com, ???@??.com"
+#export ="???@??.com, ???@??.com"
+#export ="???@??.com, ???@??.com"
+
+#approved agencies 
+#export ="???@??.com, ???@??.com"
+#export ="???@??.com, ???@??.com"
+#export ="???@??.com, ???@??.com"
+
 echo "RUNNING...." &&
 
 docker run --restart always \
@@ -7,7 +17,8 @@ docker run --restart always \
 -e "NODE_ENV=$NODE_ENV" \
 -e "DB_HOST=$SLANE_API_DB_HOST" -e "DB_USER=$SLANE_API_DB_USER" -e "DB_PASSWORD=$SLANE_API_DB_PASSWORD" -e "DB_NAME=$SLANE_API_DB_NAME" \
 -e "EMAIL_USER=$SLANE_API_EMAIL_USER" -e "EMAIL_PASSWORD=$SLANE_API_EMAIL_PASSWORD" \
--e "CSV_REPORT_EMAILER_TO=$CSV_REPORT_EMAILER_TO" -e "CSV_REPORT_EMAILER_CC=$CSV_REPORT_EMAILER_CC" -e "CSV_REPORT_EMAILER_BCC=$CSV_REPORT_EMAILER_BCC" \
+-e "CSV_REPORT_EMAILER_STUDENT_APP_TO=$CSV_REPORT_EMAILER_STUDENT_APP_TO" -e "CSV_REPORT_EMAILER_STUDENT_APP_CC=$CSV_REPORT_EMAILER_STUDENT_APP_CC" -e "CSV_REPORT_EMAILER_STUDENT_APP_BCC=$CSV_REPORT_EMAILER_STUDENT_APP_BCC" \
+-e "CSV_REPORT_EMAILER_APPROVED_AGENCY_TO=$CSV_REPORT_EMAILER_APPROVED_AGENCY_TO" -e "CSV_REPORT_EMAILER_APPROVED_AGENCY_CC=$CSV_REPORT_EMAILER_APPROVED_AGENCY_CC" -e "CSV_REPORT_EMAILER_APPROVED_AGENCY_BCC=$CSV_REPORT_EMAILER_APPROVED_AGENCY_BCC" \
 -p 41965:3000 -d --name application_csv_emailer student_application_csv_emailer &&
 
 echo "DONE you can test it by running ' curl -i localhost:41965/api/healthcheck '" && 
