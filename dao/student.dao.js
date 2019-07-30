bb5784343c22249d2d8f701a66279c82f847bb55
Jay Bengco <jay.bengco@tooltwist.com>
2019-07-30 09:49:13 +0800

    add student id and course application status'

--- dao/student.dao.js
+++ dao/student.dao.js
@@ -533,7 +533,7 @@ function getStudentLatestChangesApplicationWeeklyPotentialInvoice() {
     });
 }
 
-function getCourseApplicationsWithoutInvoice(){
+function getCourseApplicationsWithoutInvoice() {
     return new Promise((resolve, reject) => {
         database.getConnection((err, connection) => {
             let sql = `
@@ -577,11 +577,13 @@ async function getStudentsWithStudyCommencedCourseApplication() {
         database.getConnection((err, connection) => {
             const sql = `
                 SELECT DISTINCT 
+                IFNULL(student.id, '') 'Student Id',
                 IFNULL(student.firstname, '') 'Student First Name',
                 IFNULL(student.lastname, '') 'Student Last Name',
                 IFNULL(student.email, '') 'Email',
                 IFNULL(telephone.number, '') 'Contact No.',
                 IFNULL(course.course_name, '') 'Course Name',
+                IFNULL(course_application_status.label, '') 'Course Application Status',
                 IFNULL(provider.provider_name, '') 'Institution',                    
                 IFNULL(campus.campus_name, '') 'Campus Name',
                 IFNULL(campus_location_details.country_code, '') 'Campus Country',
@@ -609,6 +611,8 @@ async function getStudentsWithStudyCommencedCourseApplication() {
                         ON course_application.student_id = student.id 
                 INNER JOIN course 
                         ON course_application.course_id = course.course_id 
+                INNER JOIN course_application_status
+                        ON course_application.course_application_status_id = course_application_status.id
                 INNER JOIN provider 
                         ON course.provider_id = provider.provider_id 
                 INNER JOIN campus 
@@ -706,7 +710,7 @@ function getLatestStudentsWithOrWithoutCourseApplicationFromSundayToSaturday() {
                 ) 
                 `;
 
-        
+
 
             sql += ' ORDER BY course_application.date_created, student.date_created DESC';
             return connection.query(sql, params, (err, resultSet) => {
